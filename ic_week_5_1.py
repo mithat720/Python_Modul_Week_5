7cd10e3e5b530bac7095b85238eeba58999c7672
icubuk <ibrahimcubuk@hotmail.com>
2025-05-14 23:42:09 +0200

    Create ic_week_5_1.py

---
+++ ic_week_5_1.py
@@ -0,0 +1,26 @@
+class Rectangle:
+    def __init__(self, breedte, hoogte):
+        self.breedte = breedte
+        self.hoogte = hoogte
+        self.oppervlakte = self.breedte * self.hoogte
+        self.omtrek = 2 * (self.breedte + self.hoogte)
+    
+    #def oppervlakte(self):
+    #    return self.breedte * self.hoogte
+    #def omtrek(self):
+    #    return 2*(self.breedte+self.hoogte)
+
+
+r = Rectangle(5, 7)
+breedte = 15
+
+#print("Alanı", r.oppervlakte())
+#print("Çevresi", r.omtrek())
+
+
+print(r.oppervlakte)
+print(r.omtrek)
+
+
+
+
