edfb321c701e264e95ea4c2f67c8aa81bf04d67b
icubuk <ibrahimcubuk@hotmail.com>
2025-05-15 18:25:09 +0200

    Update ic_week_5_1.py

--- ic_week_5_1.py
+++ ic_week_5_1.py
@@ -4,23 +4,17 @@ class Rectangle:
         self.hoogte = hoogte
         self.oppervlakte = self.breedte * self.hoogte
         self.omtrek = 2 * (self.breedte + self.hoogte)
-    
+   
     #def oppervlakte(self):
     #    return self.breedte * self.hoogte
     #def omtrek(self):
     #    return 2*(self.breedte+self.hoogte)
 
-
-r = Rectangle(5, 7)
-breedte = 15
+r=Rectangle(5, 7)
+r.breedte = 25
 
 #print("Alanı", r.oppervlakte())
 #print("Çevresi", r.omtrek())
 
-
 print(r.oppervlakte)
 print(r.omtrek)
-
-
-
-
